74e6d5de02796026f74da6726a88880b38c5174b
Nikolay Vasilev <lexuspexus@yandex.ru>
2022-12-13 17:19:03 +0300

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
+
         int creditAmount = 1_000_000;
         int creditTerm = 1;
         double percent = 9.99;
-        int creditPayment = service.calculate(creditAmount, creditTerm, percent);
-        System.out.println("Ежемесячный платёж - " + creditPayment + " руб.");
+        int creditPayment1 = service.calculate(creditAmount, creditTerm, percent);
+        System.out.println("Ежемесячный платёж - " + creditPayment1 + " руб.");
+
+        creditTerm = 2;
+        creditPayment1 = service.calculate(creditAmount, creditTerm, percent);
+        System.out.println("Ежемесячный платёж - " + creditPayment1 + " руб.");
+
+        creditTerm = 3;
+        creditPayment1 = service.calculate(creditAmount, creditTerm, percent);
+        System.out.println("Ежемесячный платёж - " + creditPayment1 + " руб.");
 
     }
 }
